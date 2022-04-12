0b36504f7588b9117142f57d799aae384778d379
Ryan Day <radday1@gmail.com>
2022-04-12 14:26:20 -0700

    Adds controllers and routes

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,36 @@
+class PlacesController < ApplicationController
+  def index
+    places = Place.all
+    render json: place.as_json
+  end
+
+  def create
+    place = Place.new(
+      name: params[:name],
+      address: params[:address],
+           
+    )
+    place.save
+    render json: place.as_json
+  end
+
+  def show
+    place = Place.find_by(id: params[:id])
+    render json: place.as_json
+  end
+
+  def update
+    place = Place.find_by(id: params[:id])
+    place.name = params[:name] ||place.name
+    place.width = params[:width] || place.width
+    place.save
+    render json: place.as_json
+  end
+
+  def destroy
+    place = Place.find_by(id: params[:id])
+    place.destroy
+    render json: {message: "Place successfully destroyed."}
+  end
+end
+end
